b8835ec6bc2773591b91362892068576f56c5678
Barry <96130733+Bouba-Barry@users.noreply.github.com>
2023-07-06 02:16:15 +0100

    updating the backend Response

--- src/components/AuthContent.js
+++ src/components/AuthContent.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { callApi } from '../helpers/axios_helper';
 import { toast } from 'react-toastify';
+import { getUser } from '../helpers/auth_helper';
 
 
 
 export default function AuthContent(props){
-
 
     const [apiResponse, setApiResponse] = useState("");
 
@@ -13,21 +13,23 @@ export default function AuthContent(props){
         handleCallApi();
     },[]);
 
-    const handleCallApi = () => {
-        try {
-            if (props.user) {
-              const response = callApi();
-              setApiResponse(response.data);
-
-              toast.success('Api returned data successfully, check in section - Api response');
-              console.log("dataResponse : : "+response.data);
-            } else {
-              toast.info('You are not logged in.');
-            }
-          } catch (error) {
-            toast.error(error.message);
-          }
+    const handleCallApi = async () => {
+      try {
+        const user = getUser();
+        if (user) {
+          const response = await callApi();
+          setApiResponse(response.data);
+    
+          toast.success('Api returned data successfully, check in section - Api response');
+          console.log("dataResponse : : " + response.data);
+        } else {
+          toast.info('You are not logged in.');
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
     };
+    
     return (
         <div className="row justify-content-md-center">
             <div className="col-4">
